b09754b49eb451d6cc61a8848af151212544827e
ishi-0409 <isaku0409@icloud.com>
2026-07-19 02:05:01 +0900

    change rds

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -27,8 +27,9 @@ resource "aws_db_instance" "library" {
   identifier             = "${var.project_name}-db"
   engine                 = "postgres"
   engine_version         = "16"
-  instance_class         = "db.t3.micro"
+  instance_class         = "db.t4g.micro"
   allocated_storage      = 20
+  storage_type           = "gp3"
   db_name                = var.db_name
   username               = var.db_username
   password               = var.db_password
