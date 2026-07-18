5d6735069d7c4cfeae4455328312c5959d67fe8d
ishi-0409 <isaku0409@icloud.com>
2026-07-19 02:21:02 +0900

    change rds

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -30,6 +30,7 @@ resource "aws_db_instance" "library" {
   instance_class         = "db.t4g.micro"
   allocated_storage      = 20
   storage_type           = "gp3"
+  apply_immediately      = true 
   db_name                = var.db_name
   username               = var.db_username
   password               = var.db_password
